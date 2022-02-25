f4acc90ed2e0a52d1e8beeccf5a1d226f95c138c
AlanZhang2002 <alanmz2002@gmail.com>
2022-02-24 23:08:39 -0500

    started sorting food items based on station, WIP due to site not in a normal working state over break

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import requests
 import bs4
 import lxml
-import _ctypes
+import discord
 
-
-def get_food_list(tag):  # TODO: sort by Rosie's, Sizzle, etc
+def get_food_list(tag, num):  # TODO: sort by Rosie's, Sizzle, etc. Maybe add some
     food_elements = tag.find(id=tag.find(class_="c-tab__list site-panel__daypart-tab-list")
                              .button.attrs['aria-controls']).div.find_all(class_="h4 site-panel__daypart-item-title")
-    items = []
-    for i in food_elements:
+    station_elements = tag.find(id=tag.find(class_="c-tab__list site-panel__daypart-tab-list")
+                             .button.attrs['aria-controls']).div.find_all(class_="site-panel__daypart-item-station")
+
+    items = [[]] # separated by station
+
+    for i in range(len(station_elements)):
+        if station_elements[i].getText() == '@roots':
+            items[0].append(' '.join(food_elements[i].getText().split()))
+        elif station_elements[i].getText() == '@sizzle': # @rosies favorites | @pomodoro | @kettles | @market || @rise | @roots || there are probably more, check different days
+
         items.append(' '.join(i.getText().split()))  # parses string, removes excess \n and \t
     return items
 
 
 def get_menu():
-    global res
+    global res, lunch, dinner
     res = requests.get('https://rose-hulman.cafebonappetit.com/')
     print(type(res))
     try:
         res.raise_for_status()
         soup = bs4.BeautifulSoup(res.text, 'lxml')
+        breakfast = soup.select('#breakfast')[0]
+        brunch = soup.select('#brunch')[0]
         lunch = soup.select('#lunch')[0]
         dinner = soup.select('#dinner')[0]
 
-        lunch_items = get_food_list(lunch)
-        dinner_items = get_food_list(dinner)
+        breakfast_items = get_food_list(breakfast, 0)
+
+        lunch_items = get_food_list(lunch, 1)
+        dinner_items = get_food_list(dinner, 2)
 
 
     except Exception as exc:
@@ -33,4 +44,4 @@ def get_menu():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    get_menu()
